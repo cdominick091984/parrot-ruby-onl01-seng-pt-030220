89aefcbb1c5a473994fcb1ce641882eb32ed1ffa
Domenico Cascone <dcascone84@gmail.com>
2020-03-03 14:16:06 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(parrot_sound = "Squawk!")
-  puts "Polly want a #{parrot_sound}"
+  puts "#{parrot_sound}"
 end
